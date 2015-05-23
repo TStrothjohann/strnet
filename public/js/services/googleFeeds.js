8ab25938a9ff161ac47dfeac06b6a42e1accc835
Thomas Strothjohann <thomas@strothjohann.net>
2015-05-23 15:43:03 +0200

    torialCard parses torialRSS to get photo.

--- public/js/services/googleFeeds.js
+++ public/js/services/googleFeeds.js
@@ -1,7 +1,7 @@
 var googleFeeds = function($resource){
   return $resource('http://ajax.googleapis.com/ajax/services/feed/load', {}, {
-      fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
-    });
+    fetch: { method: 'JSONP', params: {v: '1.0', callback: 'JSON_CALLBACK'} }
+  });
 };
 
 angular
--- public/js/services/torialCard.js
+++ public/js/services/torialCard.js
@@ -1,18 +1,18 @@
 var torialCardWrapper = function (googleFeeds, $sanitize) {
+  
   var torialCard = function(index){
     if(index === undefined){index = 0}
     var self = this
     this.initialize = function() {
-      var feedSource = {title: 'Slashdot', url: 'https://www.torial.com/thomas.strothjohann.atom'};
+      var feedSource = {title: 'Torial', url: 'https://www.torial.com/thomas.strothjohann.atom'};
       googleFeeds.fetch({q: feedSource.url, num: 20}, {}, function (data) {
-      
-      self.textHTML = data.responseData.feed.entries[index].content
-      
+      var parsed = jQuery.parseHTML($sanitize(data.responseData.feed.entries[index].content))
+      self.text = data.responseData.feed.entries[index].contentSnippet
       self.link = data.responseData.feed.entries[index].link
       self.title = data.responseData.feed.entries[index].title
       self.date = new Date(data.responseData.feed.entries[index].publishedDate).toDateString();
       self.author = data.responseData.feed.entries[index].author
-        
+      self.photo = parsed[0].children[0].children[0].children[0].children[0].src
       });
     };
     this.initialize();
